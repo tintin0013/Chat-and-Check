57ffe279c54e840e0ec53b76624e0dc8586e6436
Arnaud Pinatel <tintin0013@gmail.com>
2026-05-29 21:09:23 +0200

    Add initial project skeleton

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", startExperience);
+
+function startExperience() {
+    console.log("Début de l'expérience");
+}
